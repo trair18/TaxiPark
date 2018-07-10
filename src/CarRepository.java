3510432eeb9b2b489be58e26a3e620c8d99f65da
Artem Dorohovich <trair8@gmail.com>
2018-07-10 21:45:17 +0300

    update CarRepository

--- src/CarRepository.java
+++ src/CarRepository.java
@@ -6,38 +6,35 @@ import java.util.Scanner;
 public class CarRepository {
     public ArrayList<Car> findAll(){
         ArrayList<Car> cars = new ArrayList<Car>();
-        String type;
-        int id;
-        String name;
-        double fuelConsumption;
-        int speed;
         try {
             Scanner scanner = new Scanner(new File("cars.txt"));
             while (scanner.hasNextLine()){
                 String[] s = scanner.nextLine().split(", ");
-                type = s[0];
-                id = Integer.valueOf(s[1]);
-                name = s[2];
-                fuelConsumption = Double.valueOf(s[3]);
-                speed = Integer.valueOf(s[4]);
-                switch (type){
-                    case "car":
-                        cars.add(new Car(id, name, fuelConsumption, speed));
-                        break;
-                    case "premium car":
-                        boolean wifi = Boolean.valueOf(s[5]);
-                        boolean monitor = Boolean.valueOf(s[6]);
-                        cars.add(new PremiumCar(id, name, fuelConsumption, speed, wifi, monitor));
-                        break;
-                    case "mini bus":
-                        String function = s[5];
-                        cars.add(new MiniBus(id, name, fuelConsumption, speed, function));
-                        break;
-                }
+                cars.add(createCar(s));
             }
         }catch (FileNotFoundException ex){
             ex.printStackTrace();
         }
         return cars;
+    }
+
+    private Car createCar(String[] s){
+        String type = s[0];
+        int id = Integer.valueOf(s[1]);
+        String name = s[2];
+        double fuelConsumption = Double.valueOf(s[3]);
+        int speed = Integer.valueOf(s[4]);
+        switch (type){
+            case "car":
+                return new Car(id, name, fuelConsumption, speed);
+            case "premium car":
+                boolean wifi = Boolean.valueOf(s[5]);
+                boolean monitor = Boolean.valueOf(s[6]);
+                return new PremiumCar(id, name, fuelConsumption, speed, wifi, monitor);
+            case "mini bus":
+                String function = s[5];
+                return new MiniBus(id, name, fuelConsumption, speed, function);
+        }
+        return null;
     }
 }
